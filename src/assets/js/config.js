5d99b3d27b065404bb86f4d4616bcbb15466a7bb
pht <956077081@qq.com>
2021-03-20 15:30:43 +0800

    market add

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,7 +1,7 @@
 const STATIC_CONFIG = {
-   server: 'http://127.0.0.1:8081',
+   server: 'http://182.92.204.169:8081',
    version:'1.0.0',
    compCode:'QMKJ',
-   company: '银杉金服'
+   company: '千盟科技'
 }
 export { STATIC_CONFIG } ;
