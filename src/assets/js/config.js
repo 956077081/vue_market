782620588fc5c5b693294737b935126ec997041b
pht <956077081@qq.com>
2021-06-16 17:48:54 +0800

    适应 springBoot项目

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,7 +1,8 @@
 const STATIC_CONFIG = {
-  server: 'http://182.92.204.169:8081',
+  server: 'http://127.0.0.1:8081',
    version:'1.0.0',
    compCode:'QMKJ',
    company: '千盟科技'
 }
 export { STATIC_CONFIG } ;
+
